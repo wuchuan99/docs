1bd53e7f1df66be5d1734cb999ed718678d51e77
os.wuchuan <654707838@qq.com>
2021-07-17 17:28:13 +0800

    发布到github.io

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,31 +2,32 @@ const moment = require('moment');
 moment.locale('zh-cn')
 
 module.exports = {
-    plugins: [
-        [
-          '@vuepress/last-updated',
-          {
-            transformer: timestamp => {
-              return moment(timestamp).format('LLLL')
-            }
+  base: '/docs/',
+  plugins: [
+      [
+        '@vuepress/last-updated',
+        {
+          transformer: timestamp => {
+            return moment(timestamp).format('LLLL')
           }
-        ]
-    ],
-    themeConfig: {
-        lastUpdated: '更新时间',
-        logo: '/assets/img/logo.png',
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/about/' },
-            { text: 'External', link: 'https://google.com' },
-            {
-                text: 'Languages',
-                ariaLabel: 'Language Menu',
-                items: [
-                  { text: 'Chinese', link: '/language/chinese/' },
-                  { text: 'Japanese', link: '/language/japanese/' }
-                ]
-              }
-        ]
-    }
+        }
+      ]
+  ],
+  themeConfig: {
+      lastUpdated: '更新时间',
+      logo: '/assets/img/logo.png',
+      nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/about/' },
+          { text: 'External', link: 'https://google.com' },
+          {
+              text: 'Languages',
+              ariaLabel: 'Language Menu',
+              items: [
+                { text: 'Chinese', link: '/language/chinese/' },
+                { text: 'Japanese', link: '/language/japanese/' }
+              ]
+            }
+      ]
+  }
 }
